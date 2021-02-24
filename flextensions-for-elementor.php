0622dc5cdf13a525078b9af510655b98bc541aae
Felix Herzog <alexander@felixherzog.de>
2021-02-24 22:08:30 +0100

    First Extension Files

---
+++ flextensions-for-elementor.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Flextensions for Elementor WordPress Plugin.
+ *
+ * @package Flextensions
+ *
+ * Plugin Name: Flextensions for Elementor
+ * Description: Widgets 
+ * Plugin URI:  https://github.com/fleks/flextensions-for-elemento
+ * Version:     1.0.0
+ * Author:      Felix Herzog
+ * Text Domain: flex-widgets
+ */
+
+define( 'FLEXTENSIONS', __FILE__ );
+
+/**
+ * Include the Flextensions_Widgets class.
+ */
+require plugin_dir_path( FLEXTENSIONS ) . 'class-flextensions-widgets.php';
+
+add_action( 'elementor/elements/categories_registered', 'add_flextensions_category' );
+function add_flextensions_category( $elements_manager ) {
+
+    // New category array
+	$categories = [];
+    // Create flextensions category
+	$categories['flextensions-category'] =
+		[
+			'title' => 'Flextensions',
+			'icon'  => 'fa fa-plug'
+		];
+
+    // Merge Flextensions category with remaining categories
+	$old_categories = $elements_manager->get_categories();
+	$categories = array_merge($categories, $old_categories);
+	$set_categories = function ( $categories ) {
+		$this->categories = $categories;
+	};
+	$set_categories->call( $elements_manager, $categories );	
+}
