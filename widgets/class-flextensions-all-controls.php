8eda704422c095d27307f7fdaf5e391c898b5787
Felix Herzog <mail@felixherzog.de>
2022-06-09 17:21:15 +0200

    Update class-flextensions-all-controls.php

--- widgets/class-flextensions-all-controls.php
+++ widgets/class-flextensions-all-controls.php
@@ -123,7 +123,7 @@ class Flextensions_AllControls extends Widget_Base {
 	 *
 	 * @access protected
 	 */
-	protected function _register_controls() {
+	protected function register_controls() {
 		$this->start_controls_section(
 			'section_content',
 			[
