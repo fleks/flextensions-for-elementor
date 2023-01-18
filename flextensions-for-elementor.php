0fbd8a7d6c86ab1e08a80e2ccf3ae359a3748b59
Felix Herzog <mail@felixherzog.de>
2023-01-18 11:46:59 +0100

    Version 1.0.5 > 1.0.6

--- flextensions-for-elementor.php
+++ flextensions-for-elementor.php
@@ -7,7 +7,7 @@
  * Plugin Name: Flextensions for Elementor
  * Description: Widgets 
  * Plugin URI:  https://github.com/fleks/flextensions-for-elementor
- * Version:     1.0.5
+ * Version:     1.0.6
  * Author:      Felix Herzog
  * Text Domain: flextensions
  */
