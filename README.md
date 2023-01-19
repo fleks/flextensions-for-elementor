c9872f90adfee6a326a7f73ee03675921c3016c1
fleks <mail@felixherzog.de>
2023-01-19 12:31:12 +0100

    SVG Support

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # flextions-for-elementor
 Widgets for Elementor
 
+Change Log:
+1.0.8 - 2023-01-19: SVG Support
+
 [more @ flextensions.com](https://flextensions.com)
--- flextensions-for-elementor.php
+++ flextensions-for-elementor.php
@@ -7,7 +7,7 @@
  * Plugin Name: Flextensions for Elementor
  * Description: Widgets 
  * Plugin URI:  https://github.com/fleks/flextensions-for-elementor
- * Version:     1.0.7
+ * Version:     1.0.8
  * Author:      Felix Herzog
  * Text Domain: flextensions
  */
